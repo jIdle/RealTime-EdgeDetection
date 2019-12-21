1180b3a8bba7d65978289abe84ed1866cc8cf365
Kobe Davis <kodavis@pdx.edu>
2019-12-21 14:18:36 -0800

    Added functionality to display camera feed
    The program now utilizes  OpenCV's VideoCapture class to grab frames from the camera feed and display them to the screen.

--- RealTime-EdgeDetection/RealTime-EdgeDetection.cpp
+++ RealTime-EdgeDetection/RealTime-EdgeDetection.cpp
@@ -2,8 +2,33 @@
 //
 
 #include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	int keyPress = 0;
+	VideoCapture cam(0);
+	namedWindow("Camera Feed");
+	if (!cam.isOpened()) {
+		cout << "No default camera. Exiting program..." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	for (;;) {
+		Mat frame;
+		cam >> frame;
+		imshow("Camera Feed", frame);
+
+		keyPress = waitKey(1);
+		if (keyPress % 256 == 27) {
+			cout << "Escape pressed.\nExiting program..." << endl;
+			break;
+		}
+	}
+
+	cam.release();
+	destroyAllWindows();
 }
