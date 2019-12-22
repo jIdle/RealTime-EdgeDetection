6969838eff82d398370e4922ea2aacee9724e241
Kobe Davis <kodavis@pdx.edu>
2019-12-21 16:22:47 -0800

    Added functionality to apply Laplacian and Sobel filters
    The program will now process the image from the camera with either a Laplacian filter or a Sobel filter. Currently the Laplacian filter seems to work better, but it is definitely picking up more noise.

--- RealTime-EdgeDetection/RealTime-EdgeDetection.cpp
+++ RealTime-EdgeDetection/RealTime-EdgeDetection.cpp
@@ -19,15 +19,21 @@ int main()
 
 	for (;;) {
 		// Retrieve image from camera
-		Mat frame;
+		Mat frame, frame2;
+		Mat gradX, gradY;
+		Mat result;
 		cam >> frame;
 
 		// Modify image
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
-		GaussianBlur(frame, frame, Size(9,9), 2); // stdev=2 -> 9x9
+		GaussianBlur(frame, frame, Size(9,9), 2);
+		//Sobel(frame, gradX, -1, 1, 0);
+		//Sobel(frame, gradY, -1, 0, 1);
+		//addWeighted(abs(gradX), 0.5, abs(gradY), 0.5, 0, result);
+		Laplacian(frame, result, -1, 5);
 
 		// Display image
-		imshow("Camera Feed", frame);
+		imshow("Camera Feed", result);
 
 		keyPress = waitKey(1);
 		if (keyPress % 256 == 27) {
